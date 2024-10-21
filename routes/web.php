a05188bb078d153ddeaefaecae8c9fe8cccd2ed0
fredriexPramana <22410100002@dinamika.ac.id>
2024-10-21 23:29:53 +0700

    second

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-    // return 'hello world';
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Fredriex Pramana'], ['title' => 'About']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact',['title' => 'Contact']);
 });
